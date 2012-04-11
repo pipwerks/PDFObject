7a915c810a1b09cd96e1226a121a1b4a2c9ec228
Carlos Alexandro Becker <caarlos0@gmail.com>
2012-04-11 18:40:34 -0300

    fixing #16, #17: return issue.

--- pdfobject.js
+++ pdfobject.js
@@ -117,10 +117,10 @@ var PDFObject = function (obj){
 
     };
 
-	// tell if the browser is the fucking ie9
-	isIE9 = function(){
-		return (navigator.userAgent.indexOf("Trident/5") > -1);
-	}
+    // tell if the browser is the fucking ie9
+    isIE9 = function(){
+        return (navigator.userAgent.indexOf("Trident/5") > -1);
+    };
 
     //If setting PDF to fill page, need to handle some CSS first
     setCssForFullWindowPdf = function (){
@@ -228,15 +228,14 @@ var PDFObject = function (obj){
 
         }
 		
-		// if the browser is an ie9, I use a iframe, because object actually dont work as expected.
-		if(isIE9()){
-			targetNode.innerHTML = '<iframe    src="' +url +'" width="' +width +'" height="' +height +'"></iframe>';
-		} else {
-			targetNode.innerHTML = '<object    data="' +url +'" type="application/pdf" width="' +width +'" height="' +height +'"></object>';
-		}
-
-        return targetNode.getElementsByTagName("object")[0];
-
+	// if the browser is an ie9, I use a iframe, because object actually dont work as expected.
+	if(isIE9()){
+		targetNode.innerHTML = '<iframe    src="' +url +'" width="' +width +'" height="' +height +'"></iframe>';
+		return targetNode.getElementsByTagName("iframe")[0];
+	} else {
+		targetNode.innerHTML = '<object    data="' +url +'" type="application/pdf" width="' +width +'" height="' +height +'"></object>';
+		return targetNode.getElementsByTagName("object")[0];
+	}
     };
 
     //The hash (#) prevents odd behavior in Windows
