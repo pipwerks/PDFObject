e9b9be99ddaf708cce4efe712a70aa36997df3c9
Philip Hutchison <philip@pipwerks.com>
2020-09-18 22:49:12 -0700

    Update version and copyright date

--- pdfobject.js
+++ pdfobject.js
@@ -2,10 +2,10 @@
 //jshint unused:false, strict: false
 
 /**
- *  PDFObject v2.1.1
+ *  PDFObject v2.2
  *  https://github.com/pipwerks/PDFObject
  *  @license
- *  Copyright (c) 2008-2018 Philip Hutchison
+ *  Copyright (c) 2008-2020 Philip Hutchison
  *  MIT-style license: http://pipwerks.mit-license.org/
  *  UMD module pattern from https://github.com/umdjs/umd/blob/master/templates/returnExports.js
  */
