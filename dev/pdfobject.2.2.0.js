13a301a0c4da13efbcb13717b47056541269e8fa
Philip Hutchison <philip@pipwerks.com>
2020-09-21 21:19:51 -0700

    Tweak for IE11

--- dev/pdfobject.2.2.0.js
+++ dev/pdfobject.2.2.0.js
@@ -239,7 +239,7 @@
                 style += "position: absolute; top: 0; right: 0; bottom: 0; left: 0; width: 100%; height: 100%;";
             }
 
-            embed.style = style;
+            embed.setAttribute("style",style); //using setAttribute to make IE11 happy
 
         }
 
