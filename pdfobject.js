0f764d1b0a188b33d21f0786a12b1de70836499d
Philip Hutchison <philip@pipwerks.com>
2021-09-30 21:24:59 -0700

    Version bump
    Incremented version number

--- pdfobject.js
+++ pdfobject.js
@@ -1,5 +1,5 @@
 /**
- *  PDFObject v2.2.6
+ *  PDFObject v2.2.7
  *  https://github.com/pipwerks/PDFObject
  *  @license
  *  Copyright (c) 2008-2021 Philip Hutchison
@@ -35,7 +35,7 @@
             return false;
     }
 
-    let pdfobjectversion = "2.2.6";
+    let pdfobjectversion = "2.2.7";
     let nav = window.navigator;
     let ua = window.navigator.userAgent;
 
