411fad7a9f20c6ab9e28d9ac9725443e1975c7c4
Philip Hutchison <philip@pipwerks.com>
2023-05-05 17:20:29 -0700

    Update pdfobject.2.2.9.js
    Old version had been loaded accidentally. This is the latest version.

--- dev/pdfobject.2.2.9.js
+++ dev/pdfobject.2.2.9.js
@@ -234,8 +234,8 @@
 
         //Allow developer to insert custom attribute on embed/iframe element, but ensure it does not conflict with attributes used by PDFObject
         let reservedTokens = ["className", "type", "title", "src", "style", "id", "allow", "frameborder"];
-        if(customAttribute && customAttribute.key && !reservedTokens.includes(customAttribute.key)){
-            el[customAttribute.key] = (typeof customAttribute.value !== "undefined") ? customAttribute.value : "";
+        if(customAttribute && customAttribute.key && reservedTokens.every(token => !customAttribute.key.includes(token))){
+            el.setAttribute(customAttribute.key, (typeof customAttribute.value !== "undefined") ? customAttribute.value : "");
         }
 
         targetNode.classList.add("pdfobject-container");
